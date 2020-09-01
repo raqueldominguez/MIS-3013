b5ae2cbc87972b2c6144d9b0461c17546c42f120
Raquel Dominguez <Raquel.A.Dominguez-1@ou.edu>
2020-09-01 11:49:46 -0500

    Created new repository with starter solutions

---
+++ Participations/Participation_SumOf3Numbers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Participation_SumOf3Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
