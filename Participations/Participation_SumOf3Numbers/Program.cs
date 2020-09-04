82ee88deedfea8f441506cbb48316aa581717f16
Raquel Dominguez <Raquel.A.Dominguez-1@ou.edu>
2020-09-04 13:52:20 -0500

    Added number inputs and sum of. Added sum multiplied by Lucky Number

--- Participations/Participation_SumOf3Numbers/Program.cs
+++ Participations/Participation_SumOf3Numbers/Program.cs
@@ -1,13 +1,26 @@
 ﻿using System;
+using System.Xml;
 
-namespace Participation_SumOf3Numbers
+namespace Participation_SumOf3
+
 {
     class Program
     {
         const double LUCKY_NUMBER = 7.777;
+            
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter a first number.");
+            int first = (int)Convert.ToDouble (Console.ReadLine());
+            Console.WriteLine("Enter a second number.");
+            int second = (int)Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter a third number.");
+            int third = (int)Convert.ToDouble(Console.ReadLine());
+
+            int sum = (first + second + third);
+            Console.WriteLine("The sum of the three numbers is: " + sum);
+
+            Console.WriteLine("The sum multiplied by 7.777 is: " + (sum * LUCKY_NUMBER));
         }
     }
 }
