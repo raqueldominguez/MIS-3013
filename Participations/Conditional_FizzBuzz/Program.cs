d7c2a3426036947e8dc589c7ed8ba83d06055063
Raquel Dominguez <Raquel.A.Dominguez-1@ou.edu>
2020-09-08 12:22:26 -0500

    added fizz, buzz, and fizz buzz if statements

---
+++ Participations/Conditional_FizzBuzz/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Conditional_FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int number = rnd.Next(1, 101);
+
+            //BOOLEAN OPERATORS:
+            //      OR = ||
+            //      AND = &&
+            
+            if (number % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+
+            if (number % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+
+            if (number % 3 == 0 && number % 5 == 0)
+            {
+                Console.WriteLine("Fizz Buzz");
+            }
+            else
+            {
+                Console.WriteLine(number);
+            }
+
+            Console.WriteLine(number);
+        }
+    }
+}
