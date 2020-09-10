4e1ff8e552e136bb2e075ad324c4099027c3c3b1
Raquel Dominguez <Raquel.A.Dominguez-1@ou.edu>
2020-09-10 11:35:18 -0500

    Added if... else if statement

--- Participations/Conditional_CoinToss/Program.cs
+++ Participations/Conditional_CoinToss/Program.cs
@@ -6,14 +6,29 @@ namespace Conditional_CoinToss
     {
         static void Main(string[] args)
         {
+            
+            Console.WriteLine("Pick heads or tails");
             Random rdn = new Random();
             int randomNumber = rdn.Next(0, 2);
             Console.WriteLine(randomNumber);
-            Console.WriteLine("Pick heads or tails");
-            string usersGuessAsString = Console.ReadLine();
-            int usersGuess;
+            int answer;
+            string answer = Console.ReadLine();
             const int HEADS = 0;
             const int TAILS =1; 
+
+            if (rdn.Next == 0)
+                Console.WriteLine("Heads");
+            else if (rdn.Next == 1)
+                Console.WriteLine("Tails");
+	{
+
+	}
+	{
+
+	}
+	{
+
+	}
 
         }
     }
