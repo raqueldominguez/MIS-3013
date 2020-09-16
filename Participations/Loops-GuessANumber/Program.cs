c953bff5087514751eb385d96937e5a8ae46098d
Raquel Dominguez <Raquel.A.Dominguez-1@ou.edu>
2020-09-15 23:17:03 -0500

    Started code inserted variables and wrote writelines asking user to pick a min and max number

---
+++ Participations/Loops-GuessANumber/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int min;
+            int max;
+            Random rnd = new Random();
+            int randomNumber = rnd.Next(min,max);
+
+            Console.WriteLine("Pick a minimum number.");
+            Console.ReadLine();
+            Console.WriteLine("Pick a maximum number");
+            Console.ReadLine();
+
+        }
+    }
+}
