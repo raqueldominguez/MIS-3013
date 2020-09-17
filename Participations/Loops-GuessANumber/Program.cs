322a059cb0633464c8e2b69899148ab8c0d72160
Raquel Dominguez <Raquel.A.Dominguez-1@ou.edu>
2020-09-17 12:02:18 -0500

    Created and finished random guess using do while loop for the user to input min and max value and guess random number

--- Participations/Loops-GuessANumber/Program.cs
+++ Participations/Loops-GuessANumber/Program.cs
@@ -11,16 +11,32 @@ namespace GuessNumber
     {
         static void Main(string[] args)
         {
-            int min;
-            int max;
-            Random rnd = new Random();
-            int randomNumber = rnd.Next(min,max);
+            int lowerValue;
+            int upperValue;
+            int userGuess;
 
             Console.WriteLine("Pick a minimum number.");
-            Console.ReadLine();
+            lowerValue = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Pick a maximum number");
-            Console.ReadLine();
+            upperValue = Convert.ToInt32(Console.ReadLine());
 
+            Random rnd = new Random();
+            int randomNumber = rnd.Next(lowerValue, upperValue + 1);
+
+            do
+            {
+                Console.WriteLine("Enter your guess for the random number.");
+                userGuess = Convert.ToInt32(Console.ReadLine());
+               
+                if (userGuess > upperValue || userGuess< lowerValue)
+                {
+                    Console.WriteLine("Sorry your number is outside the range. Try again.");
+                }
+            } while (userGuess != randomNumber);
+            Console.WriteLine("Congratulations, that is the correct guess!");
+           
+            
+            Console.ReadKey();
         }
     }
 }
