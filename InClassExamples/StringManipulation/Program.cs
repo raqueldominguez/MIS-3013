26a5a1097837754431f47e4d4fd9aee5cf744a55
Raquel Dominguez <Raquel.A.Dominguez-1@ou.edu>
2020-09-22 12:15:59 -0500

    Created code for string manipulations

---
+++ InClassExamples/StringManipulation/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringManipulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //                         012345678910 
+            String instanceOfString = "Hello World!";
+
+            instanceOfString = instanceOfString.ToUpper();
+            for (int i = 0; i < instanceOfString.Length; i++)
+            {
+                char letter = instanceOfString[i];
+                // Have to convert the CHARACTER to a string to use the string methods
+                Console.WriteLine(letter);//.ToString().ToUpper());
+            }
+
+            Console.ReadKey();
+        }
+        
+    }
+}
