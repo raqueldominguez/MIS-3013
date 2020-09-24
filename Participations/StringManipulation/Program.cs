e22c8aa8f802b2216a915c6ca79ef978f9c1ac54
Raquel Dominguez <Raquel.A.Dominguez-1@ou.edu>
2020-09-24 14:35:58 -0500

    Changed wording for questions outputed to user

--- Participations/StringManipulation/Program.cs
+++ Participations/StringManipulation/Program.cs
@@ -13,7 +13,7 @@ namespace StringManipulation
             string s1 = "Programming today is a race between software engineers striving to build bigger and better idiot-proof programs, and the universe trying to build bigger and better idiots. So far, the universe is winning.";
             Console.WriteLine(s1);
             
-            Console.WriteLine("What word do you want to look for in the above sentence?");
+            Console.WriteLine(">>What word do you want to look for in the above sentence?");
             string word = Console.ReadLine();
 
             Console.WriteLine($"What word do you want to change {word} to?");
