8f0a16cff6330ae46e0b514b0ea125fa241637a7
Raquel Dominguez <Raquel.A.Dominguez-1@ou.edu>
2020-09-30 10:55:46 -0500

    Created file for collections participation

---
+++ Participations/Collections_Min_Max_Avg/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Collections_Min_Max_Avg
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
