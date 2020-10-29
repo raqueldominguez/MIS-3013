070edb72f92d4c872eaa30e7e7c25aebba22aaf5
Raquel Dominguez <Raquel.A.Dominguez-1@ou.edu>
2020-10-29 12:31:34 -0500

    Did an in class example over classes with the Rectangle

---
+++ InClassExamples/Classes/Rectangle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes
+{
+    public class Rectangle
+    {
+        private int width;
+        private int height;
+
+        //this is the default/empty constructor for the class
+        public Rectangle()
+        {
+            width = 0;
+            height = 0;
+        }
+
+        public Rectangle(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public double Area()
+        {
+            double result = width * height;
+
+            return result;
+        }
+    }
+}
