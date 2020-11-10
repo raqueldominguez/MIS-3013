c998860562607280e970ddb553101c6083c75c96
Raquel Dominguez <Raquel.A.Dominguez-1@ou.edu>
2020-11-10 12:11:48 -0600

    Created classes for rectangle and circle and created instances of them in the main program

---
+++ Participations/Classes_RectangleAndCircle/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Classes_RectangleAndCircle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle myCircle = new Circle();
+            myCircle.Radius = 7.5;
+            Console.WriteLine($"The area of a circle with a radius of {myCircle.Radius} is {myCircle.CalculateArea()}\n" +
+                                $" and a perimeter of {myCircle.CalculatePerimeter()}");
+
+            Circle circle2 = new Circle();
+            circle2.Radius = 82828282;
+            double areaOfCircle2 = circle2.CalculateArea();
+            double perimeterOfCircle2 = circle2.CalculatePerimeter();
+
+            Circle circle3 = new Circle(3.5);
+
+
+            Rectangle myRectangle = new Rectangle();
+            myRectangle.Length = 4;
+            myRectangle.Width = 8;
+            Console.WriteLine($"The area of a rectangle with length of {myRectangle.Length} and width of {myRectangle.Width} is {myRectangle.CalculateArea()}\n" +
+                                $" and a perimeter of {myRectangle.CalculatePerimeter()}");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Participations/Classes_RectangleAndCircle/Rectangle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes_RectangleAndCircle
+{
+    class Rectangle
+    {
+        public double Length  { get; set; }
+        public double Width { get; set; }
+
+        public Rectangle()
+        {
+            Length = 0;
+            Width = 0;
+        }
+
+        public double CalculateArea()
+        {
+            return Width * Length;
+        }
+
+        public double CalculatePerimeter()
+        {
+            return 2 * (Length + Width);
+        }
+    }
+}
