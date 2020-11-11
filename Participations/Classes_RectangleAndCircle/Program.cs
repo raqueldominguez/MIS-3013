cb7a72df47b1cd73d5d6775e9281a8f3f9b3a542
Raquel Dominguez <Raquel.A.Dominguez-1@ou.edu>
2020-11-10 18:09:39 -0600

    Finished classes for rectangles and circles

--- Participations/Classes_RectangleAndCircle/Program.cs
+++ Participations/Classes_RectangleAndCircle/Program.cs
@@ -9,7 +9,7 @@ namespace Classes_RectangleAndCircle
             Circle myCircle = new Circle();
             myCircle.Radius = 7.5;
             Console.WriteLine($"The area of a circle with a radius of {myCircle.Radius} is {myCircle.CalculateArea()}\n" +
-                                $" and a perimeter of {myCircle.CalculatePerimeter()}");
+                                $" and a perimeter of {myCircle.CalculatePerimeter()}.");
 
             Circle circle2 = new Circle();
             circle2.Radius = 82828282;
@@ -23,7 +23,7 @@ namespace Classes_RectangleAndCircle
             myRectangle.Length = 4;
             myRectangle.Width = 8;
             Console.WriteLine($"The area of a rectangle with length of {myRectangle.Length} and width of {myRectangle.Width} is {myRectangle.CalculateArea()}\n" +
-                                $" and a perimeter of {myRectangle.CalculatePerimeter()}");
+                                $" and a perimeter of {myRectangle.CalculatePerimeter()}.");
 
             Console.ReadKey();
         }
