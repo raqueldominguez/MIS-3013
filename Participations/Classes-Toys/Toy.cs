77b48be44127d81cf6918e012bf57e5f49b7b2d9
Raquel Dominguez <Raquel.A.Dominguez-1@ou.edu>
2020-11-16 10:49:16 -0600

    Worked on get aisle and generating a random number

--- Participations/Classes-Toys/Toy.cs
+++ Participations/Classes-Toys/Toy.cs
@@ -1,6 +1,24 @@
-﻿namespace Classes_Toys
+﻿using System;
+
+namespace Classes_Toys
 {
     public class Toy
     {
+        public string Manufacturer { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        private string Notes { get; set; }
+
+        public Toy()
+        {
+
+        }
+
+        public string GetAisle()
+        {
+            Random rnd = new Random();
+            int rndNum = rnd.Next();
+            return;
+        }
     }
 }
