ffad652d8cf195280e2d4a240ccf65c44aef038f
Raquel Dominguez <Raquel.A.Dominguez-1@ou.edu>
2020-11-19 12:23:28 -0600

    started working on textbox

---
+++ Participations/TextBox/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TextBox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TextBox MyTextBox = new TextBox();
+
+            MyTextBox.Padding = 15;
+            MyTextBox.BackColor = ConsoleColor.Red;
+            MyTextBox.ForeColor = ConsoleColor.White;
+            MyTextBox.text = "Hello, world!";
+
+        }
+    }
+}
---
+++ Participations/TextBox/TextBox.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TextBox
+{
+    class TextBox
+    {
+        public int Padding { get; set; }
+        public ConsoleColor BackColor { get; set; }
+        public ConsoleColor ForeColor { get; set; }
+        public string text { get; set; }
+
+        public TextBox()
+        {
+
+        }
+
+        public void DisplayText()
+        {
+
+        }
+    }
+}
